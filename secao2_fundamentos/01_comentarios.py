7f01265dd855b4ba844f70125ca9b49ebbc91612
jgjefersonluis <jgjefersonluis@gmail.com>
2023-01-15 23:49:45 -0300

    first commit

---
+++ secao2_fundamentos/01_comentarios.py
@@ -0,0 +1,35 @@
+## Comentários
+
+# isto é um comentário
+# isto é outro comentário
+print("Olá, mundo!") # isto é um comentário
+
+''' Este é um comentário
+o comentário continua aqui
+aqui ainda continua
+e aqui termina '''
+
+print('Olá, mundo!')
+print(10)
+print(20.5)
+
+print("Maça", 20, 30.45)
+
+# sep = separar com ifem ou outro secreto
+
+print("Maça", "Pera", "Uva", sep=' - ')
+print("Maria", "Cristina", "Isaura", sep=' | ')
+print("Brasil", "Rio Grande do Sul", "Porto Alegre", sep=' * ')
+print("Maça","Pera", end = ' Fim ', sep=' ')
+print("Este é um texto longo, \n eu quero quebrar a linha")
+print("Maça","Pera", sep='\n')
+
+print("")
+
+print("A pontuação total de %s foi %s pontos" % ("Gundisalvus","10") )
+print("A pontuação total de {} foi {} pontos".format("Gundisalvus","10"))
+print("A pontuação total de" + " Gundisalvus" + " foi" + " 10 " + "pontos")
+print("")
+print("A pontuação total no Brasileirão 2022. %s foi de %s pontos." % ("Sport Club Internacional", "73") )
+print("A pontuação total no Brasileirão 2022. {} foi de {} pontos.".format("Sport Club Internacional", "73") )
+print("A pontuação total no Brasileirão 2022." + " Sport Club Internacional " + "foi de" + " 73 " + "pontos.")
---
+++ secao2_fundamentos/01_print.py
@@ -0,0 +1,17 @@
+#1 - Realize o print do seu nome completo, sua idade e sua 
+# altura utilizando um print para cada valor.
+
+print("Gundisalvus")
+print("51")
+print("1,75")
+print("116")
+
+#2 - Realize o print do seu nome completo, sua idade e sua altura 
+# utilizando apenas um print para todos valores.
+
+print("Nome: {}, idade:{}, altura:{}, peso:{}".format("Gundisalvus", "51", "1,75","120"))
+
+#3 - Realize o print de 3 números de sua escolha em um mesmo print, 
+# mas separados pelo caracter '-'.
+
+print(1,2,3, sep="-")
